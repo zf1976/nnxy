37b7b1a89792ee933b024f4f52a8c16d1ae70695
ant <1596863112@qq.com>
2020-04-03 01:54:50 +0800

    ant

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,3 +1,5 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import response.user.LoginResp;
 
 /**
@@ -6,18 +8,20 @@ import response.user.LoginResp;
  * mac
  */
 public class Test {
+
+    public static Logger LOG= LoggerFactory.getLogger(Test.class);
     public static void main(String[] args) {
         LoginResp loginResp= ApiServiceFactory.getService().login("20181004095","fengge123");
         String token=loginResp.getToken();
-        System.out.println(loginResp);
-//        System.out.println(ApiServiceFactory.getService().getDateInfo(token, "2019-10-29"));
-//        System.out.println(ApiServiceFactory.getService().getEmptyClassroomList(token, "2019-10-29", "allday"));
-//        System.out.println(ApiServiceFactory.getService().getCampusList(token));
-//        System.out.println(ApiServiceFactory.getService().getCourseInfoList(token, "20181004095", "2019-2020-1", 5));
-        System.out.println(ApiServiceFactory.getService().getCourseScoreList(token, "20161004016", "2019-2020-1"));
-//        System.out.println(ApiServiceFactory.getService().getSchoolYearList(token, "20181004095"));
-//        System.out.println(ApiServiceFactory.getService().getBuildingList(token, 3));
-//        System.out.println(ApiServiceFactory.getService().getUserInfo(token, "20181004095"));
-
+        LOG.info("{}",loginResp);
+        LOG.info("{}",ApiServiceFactory.getService().getDateInfo(token, "2019-10-29"));
+        LOG.info("{}",ApiServiceFactory.getService().getEmptyClassroomList(token, "2019-10-29", "allday"));
+        LOG.info("{}",ApiServiceFactory.getService().getCampusList(token));
+        LOG.info("{}",ApiServiceFactory.getService().getCourseInfoList(token, "20181004095", "2019-2020-1", 5));
+        LOG.info("{}",ApiServiceFactory.getService().getCourseScoreList(token, "20161004016", "2019-2020-1"));
+        LOG.info("{}",ApiServiceFactory.getService().getSchoolYearList(token, "20181004095"));
+        LOG.info("{}",ApiServiceFactory.getService().getBuildingList(token, 3));
+        LOG.info("{}",ApiServiceFactory.getService().getUserInfo(token, "20181004095"));
+        
     }
 }
