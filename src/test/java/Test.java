e21d22ba833fe665360fc1e0a95a5d451c5a6d1f
zf1976 <1596863112@qq.com>
2021-07-05 12:24:59 +0800

    feat: feat demo

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,3 +1,4 @@
+import api.param.Param;
 import lombok.extern.slf4j.Slf4j;
 import response.user.LoginResp;
 import util.ApiServiceFactory;
@@ -11,20 +12,14 @@ import util.Tools;
 @Slf4j
 public class Test {
 
+    /**
+     * 查询成绩demo
+     */
     public static void main(String[] args) {
         LoginResp loginResp= ApiServiceFactory.getService()
-                                              .login("20181004095","fengge123");
+                                              .login("20181004095","");
         String token=loginResp.getToken();
-//        LOG.info("{}",util.ApiServiceFactory.getService().getDateInfo(token, "2019-10-29"));
-//        LOG.info("{}",util.ApiServiceFactory.getService().getEmptyClassroomResult(token, "2019-10-29", "allday"));
-//        LOG.info("{}",util.ApiServiceFactory.getService().getCampusResult(token));
-//        LOG.info("{}",util.ApiServiceFactory.getService().getCourseResult(token, "20181004095", "2019-2020-1", 5));
-  //     LOG.info("{}",util.ApiServiceFactory.getService().getCourseScoreResult(token, "20181004095", ""));
- //       LOG.info("{}",util.ApiServiceFactory.getService().getSchoolYearResult(token, "20181004095"));
-//        LOG.info("{}",util.ApiServiceFactory.getService().getBuildingResult(token, 3));
-//        LOG.info("{}",util.ApiServiceFactory.getService().getUserInfo(token, "20181004095"));
-//
-        log.info("{}",Tools.getCurrentSchoolYear(token, "20181004095"));
+        log.info("{}",util.ApiServiceFactory.getService().getCourseScoreResult(token, "20181004095", Param.DATE.ALL));
     }
 
 }
